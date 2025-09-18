bcfa56229a763ede1fc2b9c8e3098828dd07eb33
masudrana2803 <masudrana2803@gmail.com>
2025-09-19 00:29:37 +0600

    edited

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -15,21 +15,26 @@ const Navbar = () => {
         <div className='flex justify-between'>
             <div>
             <Link to={'/'}>
-
         <img src="/src/Images/Nexton_Logo.png" alt="Logo" />
             </Link>
             </div>
-            <div className='flex border-2 items-center p-2 '>
+            <div className=' w-100 h-13 bg-[#F8F8F8] flex rounded-[100px] items-center p-2 '>
         <CiSearch className='mr-2' />
-        <input type="text" placeholder='Search in Products' />
+        <input type="text" placeholder='Search in Products' className='text-center w-full font-normal font-poppins text-[14px] border-none outline-none ' />
             </div>
-            <div className='flex'>
+        <div className='relative'>
+            <div className='flex gap-5 text-[32px]'>
         <CiUser />
         <CiShoppingCart />
+        </div>
+
+        <div className='absolute top-[-7px] right-[-7px] h-[24px] w-[24px] bg-[#0EA5E9] rounded-[50%] text-center' >3
+        </div>
             </div>
         </div>
         </div>
-
+        
+        
       </nav>
     </>
   )
--- src/Layouts/LayoutOne.jsx
+++ src/Layouts/LayoutOne.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import { Outlet } from 'react-router'
+import Home from '../Pages/Home'
 
 const LayoutOne = () => {
   return (
