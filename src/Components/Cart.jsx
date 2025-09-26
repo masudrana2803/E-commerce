ea92b1566ca19ee9d29cfdea4fe4cebd48428272
masudrana2803 <masudrana2803@gmail.com>
2025-09-26 17:57:29 +0600

    cartPicAdded

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -5,7 +5,7 @@ import axios from 'axios'
 
 const Cart = () => {
   
-  
+  {/* Most Important part of API integration */}
     const [Allproducts, setAllproducts] = useState([]);
   
     useEffect(() => {
@@ -16,13 +16,13 @@ const Cart = () => {
     }, []);
             console.log(Allproducts)
 
-
+{/* Most Important part of API integration */}
   return (
     <>
-      <div className='w-[340px] h-[550px] bg-blue-50 fixed top-15 right-0 mr-[20px] z-50 rounded-2xl'>
+      <div className='w-[340px] bg-blue-50 fixed top-15 right-0 mr-[20px] z-50'>
 <h2 className='text-center font-bold'>Cart</h2>
 {/* Products and prices */}
-<div className='bg-none py-2 h-[450px] overflow-y-scroll p-4'>
+<div className='bg-none h-99 py-2 overflow-y-scroll p-4'>
 
         {/* <div className='flex gap-4 items-center bg-amber-50 my-1'>
         <div className='w-12 h-12 bg-amber-600'>
@@ -33,7 +33,9 @@ const Cart = () => {
         <p className='font-poppins text-[12px] font-normal'>Price</p>
         </div>
         </div> */}
-        {Allproducts.slice(0,4).map((item) => (
+
+{/* Most Important part of API integration */}
+        {Allproducts.slice(0,10).map((item) => (
             <Productitems
               key={item.id}
               content1={item.thumbnail}
