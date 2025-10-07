bc06445e52ddc6f8f9e5a9e2f4510430e2a488a2
masudrana2803 <masudrana2803@gmail.com>
2025-10-07 23:40:10 +0600

    Settings

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -18,7 +18,6 @@ const Home = () => {
     <Recommendation/>
     <BestSellers/>
     <BannerContainer/>
-
     </>
   )
 }
--- src/Tools/CommonHead.jsx
+++ src/Tools/CommonHead.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
 
-const CommonHead = ({content1,content2}) => {
+const CommonHead = ({ content1, content2 }) => {
   return (
     <>
-      <h2 className='font-bold text-2xl font-poppins text-primary '>{content1}<span className='font-medium text-spandy'>{content2}</span></h2>
+      <h2 className="font-bold text-2xl font-poppins text-primary ">
+        {content1}
+        <span className="font-medium text-spandy"> {content2}</span>
+      </h2>
     </>
   )
 }
 
-const CommonText = ()=>{
-  return(
+export const CommonText = () => {
+  return (
     <>
-    
-    
-    
-    
+      <h2 className="font-bold text-2xl font-poppins text-primary ">
+        {content1} /</h2>
     </>
   )
 }
 
-export default {CommonHead,CommonText}
+export default CommonHead
